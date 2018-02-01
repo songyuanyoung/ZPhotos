c4b97ab3790e045504ca2ee3abc8d8b64b90de10
Zach Yang <songyuanyoung@gmail.com>
2018-02-01 14:32:07 -0600

    Master_Working branch

---
+++ app/src/main/java/com/example/zach/zphotos/HitActivity.java
@@ -0,0 +1,13 @@
+package com.example.zach.zphotos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hit);
+    }
+}
